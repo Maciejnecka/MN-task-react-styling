5691226f63a6e63b76b624ba1f7056096998195c
Maciej Necka <macieknecka@gmail.com>
2023-12-16 23:07:04 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Reset } from 'styled-reset';
+
+import App from './components/App';
+
+const root = createRoot(document.querySelector('#root'));
+root.render(
+    <>
+        <Reset />
+        <App />
+    </>,
+);
